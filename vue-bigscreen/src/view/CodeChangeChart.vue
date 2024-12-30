38a3a9784dc06a215c061fd360edcced2c3364d4
0213cc <166241538@qq.com>
2024-12-30 18:29:05 +0800

    修正

--- vue-bigscreen/src/view/CodeChangeChart.vue
+++ vue-bigscreen/src/view/CodeChangeChart.vue
@@ -1,11 +1,7 @@
 <template>
   <div id="code-change-container">
     <div class="chart-title">代码变更统计</div>
-    <div class="bg-color-black">
-      <div style="height: 100%;">
         <CodeChangeChartContainer/>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -21,28 +17,22 @@ export default {
 
 <style lang="scss" scoped>
 #code-change-container {
-  padding: 20px 16px;
   height: 100%;
   width: 100%;
-  border-radius: 5px;
   display: flex;
   flex-direction: column;
+  padding: 0 1vw;
   
   .chart-title {
-    color: #4682B4;
-    font-size: 24px;
-    margin-bottom: 10px;
+    color: #0270ca;
+    font-size: 3vh;
     text-align: center;
     font-weight: bold;
+    margin-top: -2vh;
+    margin-bottom: 2vh;
+    padding: 1vh 0;
   }
   
-  .bg-color-black {
-    flex: 1;
-    border-radius: 10px;
-    
-    > div {
-      height: 100%;
-    }
-  }
+  
 }
 </style> 
